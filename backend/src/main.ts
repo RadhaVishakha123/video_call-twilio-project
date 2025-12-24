7e7cf6a0ccfc394d55a58044d13a6c23fd17be79
Radha Vishakha <vjangir@techturbine.com>
2025-12-24 17:34:52 +0530

    change  port of postgres

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import app from './app';
-const host = process.env.HOST ?? 'localhost';
+const host = process.env.HOST ?? '0.0.0.0';
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
