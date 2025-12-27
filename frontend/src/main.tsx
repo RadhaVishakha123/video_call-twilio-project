1c36f713f411b528e26f359bfbbbfeb58e3cc95a
Radha Vishakha <vjangir@techturbine.com>
2025-12-27 16:16:11 +0530

    cookie set , and provide the refersh token

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,31 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+// import { StrictMode } from 'react';
+// import * as ReactDOM from 'react-dom/client';
+// import App from './app/app';
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'antd/dist/reset.css';
 import App from './app/app';
+import { ConfigProvider, App as AntdApp } from 'antd';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
-);
+   <ConfigProvider theme={{cssVar: {prefix: 'antd',
+      key: 'theme'}
+   }}
+   >
+    <AntdApp>
+        <App />
+      </AntdApp>
+    </ConfigProvider>
+  </StrictMode>,
+)
