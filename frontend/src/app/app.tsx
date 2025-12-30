717bde8710ac077bbfb50cd65bf114e83206ad1f
Radha Vishakha <vjangir@techturbine.com>
2025-12-30 18:30:46 +0530

    update  app.tsx

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,15 +1,13 @@
 import AuthContextProvider from '../contexts/AuthContext';
 import AppRoutes from './appRoutes';
 import { BrowserRouter } from 'react-router-dom';
-import MeetingContextProvider from '../contexts/MeetingContext';
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <AuthContextProvider>
-          <MeetingContextProvider>
-            <AppRoutes />
-          </MeetingContextProvider>
+          <AppRoutes />
         </AuthContextProvider>
       </BrowserRouter>
     </>
